09122f0caaca4e050e06013f6e76af0eedf6eef4
lumbricina <55181574+lumbricina@users.noreply.github.com>
2020-05-07 23:13:11 +0700

    Update clear.c

--- clear.c
+++ clear.c
@@ -4,11 +4,9 @@
 #include "fcntl.h"
 #include "fs.h"
 
-void help(){
-    printf(1,"clear : Membersihkan layar terminal\n");
-}
-
 int main(int argc,char *argv[])
-{   printf(1,"\033[2J\033[1;1H\n");
+{
+    
+	printf(1,"\033[2J\033[1;1H\n");
     exit();
 }
